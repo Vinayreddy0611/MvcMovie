bd32af005081efa2bc58a2a8b3cc941b4e9235fe
vinay <vinay@VINAY>
2024-02-01 12:29:35 -0500

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "Raktacharithra",
+                    ReleaseDate = DateTime.Parse("15-04-2013"),
+                    Genre = "Horror Thriller",
+                    Price = 6.85M
+                },
+                new Movie
+                {
+                    Title = "Oppenheimer ",
+                    ReleaseDate = DateTime.Parse("21-07-2023"),
+                    Genre = "Biopic Drama",
+                    Price = 39.29M
+                },
+                new Movie
+                {
+                    Title = "I saw the Devil",
+                    ReleaseDate = DateTime.Parse("30-05-2010"),
+                    Genre = "Psycho thriller",
+                    Price = 12.24M
+                },
+                new Movie
+                {
+                    Title = "Dabbe the possession",
+                    ReleaseDate = DateTime.Parse("15-04-2013"),
+                    Genre = "Horror Thriller",
+                    Price = 6.85M
+                },
+                 new Movie
+                 {
+                     Title = "Inglourious basterds",
+                     ReleaseDate = DateTime.Parse("21-08-2009"),
+                     Genre = "Adventure Drama",
+                     Price = 15.95M
+                 }
+            ); ;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MvcMovie/README.txt
@@ -0,0 +1,55 @@
+Date: 2024-01-17	
+Time: 2130
+Student ID:0850619
+
+
+STEP1:
+Created a web app using ASP.NET Core MVC and Visual studio 
+
+Started a new project called "MvcMovie" 
+Followed all the tutorials and given codes and successfully installed the visual studio.
+
+STEP2:
+Add a controller to an ASP.NET Core MVC application.
+
+added the helloworld controller and that must be added to the original project file.
+Modified the names as per my personal use.
+
+
+Comments:
+
+This lab made me realize how astonishing the web application is used and 
+it helped to learn to how to work and play with localhost and ASPNET.
+
+STEP3:
+Add a view to an ASP.NET Core MVC app
+
+Created a new folder called helloworld in the view and added the index code by following the given code.
+Changed the title,footer and menu link to make sure the output contains privacy policy with Movieapp title.
+
+STEP4:
+Add a Model
+
+Created a new class named "movie.cs" in the models folder
+Then i created the schaffolded item and completed the mvc controller entity framework.
+I didn't get any errors and got output in first attempt.
+Added the initial migration by giving following commands in the output and tested the app.
+
+STEP5:
+Work with the database
+
+I opened the SQL server object explorer from the view menu and selected the dbo.movie file to design and see the data
+Then i created the new class called SeedData and given the code but i worked on creating the new movies 
+that i like after deleting the old ones.
+
+STEP6:
+Controller action and views
+
+To showcase my favourite movies i need to get a edit and delete option in the output so that i can add the new
+movies.
+Checked all the code that i changed to verify and display the edit error of timestamp and the price which 
+provided in the workfile.
+
+
+
+
